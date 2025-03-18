8b1d8fe8582971995f73c13b4b53d771783265c5
Daniel Monteiro <158519099+danielMonteiro84@users.noreply.github.com>
2025-03-18 17:52:37 +0000

    atualizando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Em Construção</h1>;
+  return (
+    <div>
+      <h1>Estação Literária</h1>
+      <p>Em construção!!</p>
+    </div>
+  );
 }
 
 export default Home;
