72d627bfa5663b50f481c90be26015f9479b5ad8
Daniel Monteiro <158519099+danielMonteiro84@users.noreply.github.com>
2025-03-19 22:05:21 +0000

    Atualizando página home

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import React from "react";
+
 function Home() {
   return (
     <div>
-      <h1>Estação Literária</h1>
-      <p>Em construção!!</p>
+      <h1>Nação Games</h1>
+      <p>
+        Estamos preparando um espaço épico para todos os gamers! 🎮💥 Enquanto
+        finalizamos os últimos ajustes, siga-nos e fique ligado para novidades.
+        Em breve, você encontrará aqui conteúdos incríveis sobre games,
+        análises, notícias e muito mais! Aguarde... o jogo está prestes a
+        começar!
+      </p>
+      <img src="/assets/Games.jpg" alt="Games" />
     </div>
   );
 }
